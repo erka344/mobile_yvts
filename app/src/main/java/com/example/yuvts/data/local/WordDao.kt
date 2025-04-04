251035da516e39ab5043cf562a536be5e91d0215
erka344 <erdeneochirerka344@gmail.com>
2025-04-04 22:56:43 +0800

    fix some bug

--- app/src/main/java/com/example/yuvts/data/local/WordDao.kt
+++ app/src/main/java/com/example/yuvts/data/local/WordDao.kt
@@ -7,13 +7,13 @@ import kotlinx.coroutines.flow.Flow
 @Dao
 interface WordDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertWord(word: Word) // ☑ `Word` class-г параметр болгон авсан эсэхийг шалга
+    suspend fun insertWord(word: Word)
 
     @Query("SELECT * FROM words")
     fun getAllWords(): Flow<List<Word>>
 
     @Delete
-    suspend fun deleteWord(word: Word) // ☑ `Word` class байх ёстой
+    suspend fun deleteWord(word: Word)
 
     @Query("SELECT * FROM words WHERE id = :wordId")
     suspend fun getWordById(wordId: Int): Word?
--- app/src/main/java/com/example/yuvts/ui/SettingsScreen.kt
+++ app/src/main/java/com/example/yuvts/ui/SettingsScreen.kt
@@ -7,10 +7,10 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
-import androidx.compose.material3.Card
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -18,11 +18,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.yuvts.data.local.AppDatabase
-import com.example.yuvts.data.repository.WordRepository
 import com.example.yuvts.viewModel.WordViewModel
 
 @Composable
@@ -32,10 +28,9 @@ fun SettingsScreen(
     onSaveButtonClicked: () -> Unit,
     onSelectionChanged: (String) -> Unit,
 ) {
-    // Get the current language setting from the ViewModel
+
     val currentLanguage by viewModel.selectedLanguage.collectAsState(initial = "Both")
     
-    // Map the ViewModel language to the UI option
     val initialOption = when (currentLanguage) {
         "English" -> "гадаад үгийг ил харуулна"
         "Монгол" -> "монгол үгийг ил харуулна"
@@ -44,6 +39,16 @@ fun SettingsScreen(
     }
     
     var selectedOption by remember { mutableStateOf(initialOption) }
+    
+    // Update selectedOption when currentLanguage changes
+    LaunchedEffect(currentLanguage) {
+        selectedOption = when (currentLanguage) {
+            "English" -> "гадаад үгийг ил харуулна"
+            "Монгол" -> "монгол үгийг ил харуулна"
+            "Both" -> "хоёуланг нь үгийг ил харуулна"
+            else -> "хоёуланг нь үгийг ил харуулна"
+        }
+    }
     
     Column {
         listOf(
